8acc18cb5de88fb22c82aa716ee12dbdcc515d3b
Loren Siebert <loren@siebert.org>
2017-04-06 16:44:55 -0400

    Beanstalk-relate changes
    - install ruby.config for Ruby apps from https://github.com/gkop/elastic-beanstalk-ruby
    
    - enable swap
    
    - simplify Elasticsearch client config (use ELASTICSEARCH_URL envvar to connect)
    
    - yum install icu for charlock_holmes gem

--- lib/es.rb
+++ lib/es.rb
@@ -2,16 +2,8 @@ class ES
   INDEX_PREFIX = "#{Rails.env}:endpointme".freeze
 
   def self.client
-    @@client ||= Elasticsearch::Client.new(config)
+    @@client ||= Elasticsearch::Client.new(log: Rails.env == 'development')
   end
 
-  def self.config
-    config = { url: 'http://127.0.0.1:9200', log: Rails.env == 'development' }
-
-    es_yaml_file = "#{Rails.root}/config/elasticsearch.yml"
-    config.merge!(YAML.load_file(es_yaml_file).symbolize_keys) if File.exist?(es_yaml_file)
-
-    config
-  end
 end
 Elasticsearch::Model.client = ES.client
